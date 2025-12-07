7455c0c01b3d7ac925d129e9bb6cefb48b7653b0
johsua.hartwig <johsua.hartwig@estudiantes.utec.edu.uy>
2025-12-07 15:28:14 -0300

    refactor: example code reorganized for better performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ export default function Home() {
     return (
         <div className="container-1">
             <h1>Index page</h1>
+            <div className="prueba">
+                <h1>prueba 1</h1>
+                <h1>prueba 2</h1>
+            </div>
         </div>
     )
 }
